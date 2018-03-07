5e82c6824e1d3dd190516b83014e28048ad8542a
rajatvd <rajatvd@gmail.com>
2018-03-07 19:51:54 +0530

    Added setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='writefile_run',
+		version='0.1',
+		description='IPython cell magic to run a cell after writing to a file',
+		url='',
+		author='Rajat V D',
+		author_email='rajatvd@gmail.com',
+		license='MIT',
+		packages=['writefile_run'],
+		zip_safe=False)
