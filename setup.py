cf0ca4d06b188d4160e76e0a839be2fdcb03a8e5
rajatvd <rajatvd@gmail.com>
2018-03-13 00:06:48 +0530

    Added long description

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 
 from setuptools import setup
 
+long_description = """IPython cell magic which builds on top of the writefile cell magic. It also optionally runs the cell after writing it to the file."""
+
 setup(name='writefile_run',
-		version='0.2',
+		version='0.3',
 		description='IPython cell magic to write a cell to a file and run it',
+		long_description=long_description,
 		author='Rajat Vadiraj Dwaraknath',
 		url='https://github.com/rajatvd/WriteFileAndRun',
 		install_requires=['ipython'],
